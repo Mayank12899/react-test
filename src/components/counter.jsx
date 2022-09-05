7a1c923c34dc89d0a0249423c25d65339cc22fed
Mayank Srivastava <srivastavamayank679@gmail.com>
2022-09-05 20:36:56 +0530

    Added Axios

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Counter extends Component {
     state = {
@@ -13,6 +14,11 @@ class Counter extends Component {
     handleSurname(n){
         const Surname = n.target.value;
         this.setState({Surname});
+    }
+    handleSubmit(){
+        axios.get("http://localhost:5000/").then((response)=>{
+            console.log(response);
+        })
     }
     render() { 
         return (
@@ -32,7 +38,7 @@ class Counter extends Component {
                     <input className='m-2' type="text" onChange={n=>this.handleSurname()}/>
                 </div>
                 <div>
-                    <button className='m-2 btn-lg'>Submit</button>
+                    <button className='m-2 btn-lg' onClick={()=>this.handleSubmit()}>Submit</button>
                 </div>
             </div>
         </div>
