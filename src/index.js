775e8542ad2dcb083db4844d51e6fcd5cfce1f3a
Mayank12899 <43317933+Mayank12899@users.noreply.github.com>
2023-01-05 10:58:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 //import Counter from './components/counter';
 import Counter2 from "./components/counter2";
 import 'bootstrap/dist/css/bootstrap.css';
-
+console.log("Testing Jenkins")
 
 // ReactDOM.render(<Counter />, document.getElementById('root'))
 ReactDOM.render(<Counter2 />, document.getElementById('root'))
